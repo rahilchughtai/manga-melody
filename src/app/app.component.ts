ce01de9678f7a270a99aac5c92296217198ee9bb
Rahil Chughtai <rahilchughtai1@gmail.com>
2024-12-13 16:56:01 +0100

    basic navigation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,8 +1,11 @@
-import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
-import {MatButtonModule} from '@angular/material/button';
-import {MatSidenavModule} from '@angular/material/sidenav';
-import { NavbarComponent } from "./components/navbar/navbar.component";
+import { Component, computed, effect, HostListener, signal } from '@angular/core';
+import { RouterLink, RouterOutlet } from '@angular/router';
+import { MatButtonModule } from '@angular/material/button';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule, MatNavList } from '@angular/material/list';
+import { MatToolbarModule } from '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
@@ -10,12 +13,43 @@ import { NavbarComponent } from "./components/navbar/navbar.component";
     RouterOutlet,
     MatButtonModule,
     MatSidenavModule,
-    NavbarComponent
-],
+    CommonModule,
+    MatNavList,
+    MatListModule,
+    MatToolbarModule,
+    MatIconModule,
+    RouterLink
+  ],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'manga-melody';
-  showFiller = false;
+
+  private readonly screenWidth = signal(window.innerWidth);
+  private mobileWidthBreakpoint = 768;
+
+  @HostListener('window:resize', ['$event'])
+  onResize(_event: Event): void {
+    this.screenWidth.set(window.innerWidth);
+  }
+  readonly isScreenWide = computed(() => this.screenWidth() >= this.mobileWidthBreakpoint);
+
+  constructor() {
+    window.addEventListener('resize', this.updateScreenWidth);
+  }
+
+  public readonly navLinks = [
+    { path: '', text: 'Home' },
+    { path: '/search', text: 'Search' },
+    { path: '/favorites', text: 'Favorites' },
+    { path: '/cart', text: 'Cart' },
+    { path: '/orders', text: 'Orders' },
+    { path: '/profile', text: 'Profile' }
+  ];
+
+  private updateScreenWidth = () => {
+    this.screenWidth.set(window.innerWidth);
+  };
+
 }
