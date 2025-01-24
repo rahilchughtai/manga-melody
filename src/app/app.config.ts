952ab7b07683188a67cedcf8a860e9d7248f18f0
Rahil Chughtai <73351965+rahilchughtai@users.noreply.github.com>
2025-01-24 20:24:34 +0100

    add firebase (#4)

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -3,12 +3,28 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
+import { initializeApp, provideFirebaseApp } from '@angular/fire/app';
+import { getAuth, provideAuth } from '@angular/fire/auth';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
     provideAnimationsAsync(),
-    provideHttpClient(), provideAnimationsAsync(),
+    provideHttpClient(),
+    provideAnimationsAsync(),
+    provideFirebaseApp(() =>
+      initializeApp({
+        projectId: 'manga-melody',
+        appId: '1:784289949112:web:a9f673ed3c0b785d5c9b4d',
+        storageBucket: 'manga-melody.firebasestorage.app',
+        apiKey: 'AIzaSyDH37ROkc9fBAP23tU-lhMMLcQVX6v6J3s',
+        authDomain: 'manga-melody.firebaseapp.com',
+        messagingSenderId: '784289949112',
+      })
+    ),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
   ],
 };
