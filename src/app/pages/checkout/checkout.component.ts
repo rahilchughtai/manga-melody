805f0c457214b59d77dab18d2f196c30209c9734
Rahil Chughtai <rahilchughtai1@gmail.com>
2025-02-03 00:39:48 +0100

    checkout improvements

--- src/app/pages/checkout/checkout.component.ts
+++ src/app/pages/checkout/checkout.component.ts
@@ -17,6 +17,9 @@ import { MatIconModule } from '@angular/material/icon';
 import { OrderService } from '../../shared/services/order/order.service';
 import { APP_ROUTES } from '../../shared/utils/app-routes';
 import { calculateTotalAmount } from '../../shared/utils/manga-utils';
+import { NextTabButtonComponent } from '../../components/payment/next-tab-button/next-tab-button.component';
+import { SnackbarService } from '../../shared/services/snackbar/snackbar.service';
+import { ValidatorService } from 'angular-iban';
 
 interface CheckoutNavigationState {
   cartItems: CartItem[];
@@ -34,6 +37,7 @@ interface CheckoutNavigationState {
     ReactiveFormsModule,
     MatInputModule,
     MatIconModule,
+    NextTabButtonComponent,
   ],
   templateUrl: './checkout.component.html',
   styleUrl: './checkout.component.scss',
@@ -45,10 +49,11 @@ export class CheckoutComponent {
     .state as CheckoutNavigationState;
 
   public totalAmount = calculateTotalAmount(this.cartDataState.cartItems);
+  private snackService = inject(SnackbarService);
 
   public ibanForm = new FormGroup({
     iban: new FormControl('', {
-      validators: Validators.required,
+      validators: [Validators.required, ValidatorService.validateIban],
       nonNullable: true,
     }),
   });
@@ -72,14 +77,21 @@ export class CheckoutComponent {
     }),
   });
 
+  public stepIndex = 0;
+
+  incrementStepIndex() {
+    this.stepIndex += 1;
+  }
+
   public makeOrder() {
     const orderData: CheckOutData = {
       orderItems: this.cartDataState.cartItems,
       totalAmount: calculateTotalAmount(this.cartDataState.cartItems),
-      orderIBAN: this.ibanForm.value.iban,
+      orderIBAN: this.ibanForm.value.iban?.trim(),
       orderShippingAddress: this.shippingForm.value,
     };
     this.orderService.makeOrder(orderData);
+    this.snackService.openSnackBar('Order placed successfully!');
     this.router.navigate([APP_ROUTES.ORDERS]);
   }
 }
